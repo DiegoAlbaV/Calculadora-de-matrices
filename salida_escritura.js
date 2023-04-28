05259939b944d6975e4d827cd668a00ba6528437
Axelrora <131572215+Axelrora@users.noreply.github.com>
2023-04-28 15:59:11 -0600

    Update and rename salida_escritura.js to salida_escritura.py

--- salida_escritura.js
+++
@@ -1,8 +0,0 @@
-function escribirMatriz(matriz) {
-  const contenido = matriz.map((fila) => fila.join(" ")).join("\n");
-  const archivo = new Blob([contenido], {type: "text/plain"});
-  const enlace = document.createElement("a");
-  enlace.href = window.URL.createObjectURL(archivo);
-  enlace.download = "salida.txt";
-  enlace.click();
-}
