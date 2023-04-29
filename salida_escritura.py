49ab20a1ad97631b2e861cb0ce11e3ddfdf5db08
Axelrora <131572215+Axelrora@users.noreply.github.com>
2023-04-29 10:42:33 -0600

    Delete salida_escritura.py

--- salida_escritura.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def escribirMatriz(matriz):
-    contenido = "\n".join([" ".join(map(str, fila)) for fila in matriz])
-    with open("salida.txt", "w") as archivo:
-        archivo.write(contenido)
-    os.startfile("salida.txt")
