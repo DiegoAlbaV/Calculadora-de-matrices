fd820d182665fd87cfb39fff20abf5bf89aedbc6
Axelrora <131572215+Axelrora@users.noreply.github.com>
2023-04-29 21:44:39 -0600

    aun en prueba

--- Lectura y Escritura/implementacion.py
+++ Lectura y Escritura/implementacion.py
@@ -1,23 +1,38 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-with open('index.html', 'r') as f:
-    html = f.read()
+with open("index.html") as file:
+    html = file.read()
 
-soup = BeautifulSoup(html, "html.parser")
+soup = BeautifulSoup(html, 'html.parser')
 
-table = soup.find("table", {"id": "matrix"})
+table = soup.find("table", {"id": "tblmatrices"})
 
-numRows = len(table.find_all("tr"))
-numCols = len(table.find_all("tr")[0].find_all("td"))
+rows = table.find_all("tr")
+num_rows = len(rows)
+num_cols = len(rows[0].find_all("td"))
 
-matrix = []
-for row in table.find_all("tr"):
-  matrixRow = []
-  for cell in row.find_all("td"):
-    matrixRow.append(int(cell.text))
-  matrix.append(matrixRow)
+matriz = []
+for row in rows:
+    fila = []
+    for cell in row.find_all("td"):
+        valor = cell.find("input").get("value")
+        if valor:
+            try:
+                valor = int(valor)
+                fila.append(valor)
+            except ValueError:
+                print("Error: Ingrese un valor numérico")
+                exit()
+    if len(fila) != num_cols:
+        print("Error: La matriz debe tener todas sus filas con la misma cantidad de elementos")
+        exit()
+    matriz.append(fila)
 
-print(matrix)
-print("Filas:", numRows)
-print("Columnas:", numCols)
+print("Matriz: ", matriz)
+print("Número de filas: ", num_rows)
+print("Número de columnas: ", num_cols)
+
+matriz_resultante = matriz
+num_filas_resultante = num_rows
+num_columnas_resultante = num_cols
